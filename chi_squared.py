74253e18bccd30cf2e14777c7c4ebfb17f201d61
Christine Rehm <rehm.christine@gmail.com>
2015-11-23 20:38:50 -0600

    <Apply PEP8 guidelines>

--- chi_squared.py
+++ chi_squared.py
@@ -3,8 +3,10 @@ import pandas as pd
 from scipy import stats
 import collections
 
+
 # Load the reduced version of the Lending Club Dataset
-loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+loansData = pd.read_csv(
+	'https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 
 # Drop null rows
 loansData.dropna(inplace=True)
